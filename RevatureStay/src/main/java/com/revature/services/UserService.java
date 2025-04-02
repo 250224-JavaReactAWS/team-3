9fe32708ad1fd44af1965b96dc94ea0ce9162d8e
crisscde <cristophercuautzo@gmail.com>
2025-04-01 21:47:28 -0600

    Add register method to UserService
    1. Add some custom exceptions:
      * WrongEmailException
      * WrongPasswordException
    2. Add some if statements:
      * Check if there is a user with that email
      * Check if the password correspond to this user

--- RevatureStay/src/main/java/com/revature/services/UserService.java
+++ RevatureStay/src/main/java/com/revature/services/UserService.java
@@ -1,8 +1,6 @@
 package com.revature.services;
 
-import com.revature.exceptions.EmailAlreadyRegisteredException;
-import com.revature.exceptions.InvalidEmailFormatException;
-import com.revature.exceptions.InvalidPasswordException;
+import com.revature.exceptions.*;
 import com.revature.models.User;
 import com.revature.repos.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,20 +23,37 @@ public class UserService {
   public Optional<User> Register(User newUser) {
     Optional<User> user = userDAO.findUserByEmail(newUser.getEmail());
 
-    if(user.isPresent()) {
+    if (user.isPresent()) {
       throw new EmailAlreadyRegisteredException("Exist an account with that email. " + newUser.getEmail());
     }
 
-    if(!newUser.getEmail().matches(EMAIL_REGEX)) {
+    if (!newUser.getEmail().matches(EMAIL_REGEX)) {
       throw new InvalidEmailFormatException("Your email is invalid. An example of valid email is: exampleEmail1@domain.com ");
     }
 
-    if(!newUser.getPassword().matches(PASSWORD_REGEX)) {
+    if (!newUser.getPassword().matches(PASSWORD_REGEX)) {
       throw new InvalidPasswordException("Invalid Password. Must be at least 8 characters long and " +
               "need to contain one uppercase letter, one lowercase letter " +
               "and one special character(@$!%*?&)");
     }
 
     return Optional.of(userDAO.save(newUser));
+  }
+
+  public Optional<User> login(User userCredentials) {
+    Optional<User> user = userDAO.findUserByEmail(userCredentials.getEmail());
+    User userToLogin;
+
+    if (user.isEmpty()) {
+      throw new WrongEmailException("We can´t find an account with that email");
+    } else {
+      userToLogin = user.get();
+    }
+
+    if (!userToLogin.getPassword().equals(userCredentials.getPassword())) {
+      throw new WrongPasswordException("Incorrect Password");
+    }
+
+    return Optional.of(userToLogin);
   }
 }
