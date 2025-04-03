9b233ff8fa3e9cefc381f76818130358f5640f34
crisscde <cristophercuautzo@gmail.com>
2025-04-02 20:29:51 -0600

    Add Loggging
    Add two logs:
    1. INFO log for when a user is created
    2. ERROR log for when an unexpected exception occurs.

--- RevatureStay/src/main/java/com/revature/controllers/UserController.java
+++ RevatureStay/src/main/java/com/revature/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.revature.controllers;
 import com.revature.models.User;
 import com.revature.services.UserService;
 import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import java.util.Optional;
 @RequestMapping("users")
 public class UserController {
   private final UserService userService;
+  private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
   @Autowired
   public UserController(UserService userService){
@@ -27,8 +30,10 @@ public class UserController {
   public ResponseEntity<User> registerHandler(@RequestBody User user){
     Optional<User> userToBeRegister = userService.Register(user);
 
+    userToBeRegister.ifPresent(value -> logger.info("A new user was created with the id: {}", value.getUserId()));
+
     return userToBeRegister.map(value -> new ResponseEntity<>(value, HttpStatus.CREATED))
-            .orElseGet(() -> ResponseEntity.badRequest().build());
+                           .orElseGet(() -> ResponseEntity.badRequest().build() );
   }
 
   @PostMapping("login")
