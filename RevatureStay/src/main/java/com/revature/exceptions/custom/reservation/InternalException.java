9da11cf852b4054691fd0a1404ee863eb32feccd
Aldair88e <edherderek@gmail.com>
2025-04-06 22:05:23 -0600

    Added new InternalException to throw when something went wrong while performing operations over a Reservation

---
+++ RevatureStay/src/main/java/com/revature/exceptions/custom/reservation/InternalException.java
@@ -0,0 +1,7 @@
+package com.revature.exceptions.custom.reservation;
+
+public class InternalException extends RuntimeException {
+    public InternalException(String message) {
+        super(message);
+    }
+}
