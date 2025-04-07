a589f67ddc3df02286deecd90a1759ddfc537231
Aldair88e <edherderek@gmail.com>
2025-04-06 22:40:12 -0600

    Deleted HotelDAO and UnauthenticatedException to avoid merge conflicts

--- RevatureStay/src/main/java/com/revature/exceptions/custom/user/UnauthenticatedException.java
+++
@@ -1,7 +0,0 @@
-package com.revature.exceptions.custom.user;
-
-public class UnauthenticatedException extends RuntimeException {
-    public UnauthenticatedException() {
-        super("User is not authenticated");
-    }
-}
--- RevatureStay/src/main/java/com/revature/repos/HotelDAO.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repos;
-
-import com.revature.models.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HotelDAO extends JpaRepository<Hotel, Integer> {
-}
