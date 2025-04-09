b80fb5674aa56c16caa90f262f61573040e8d015
crisscde <cristophercuautzo@gmail.com>
2025-04-09 13:30:57 -0600

    Fix main.tsx

--- RevatureStayFrontend/src/main.tsx
+++ RevatureStayFrontend/src/main.tsx
@@ -1,16 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from '@mui/material/styles'
-import CssBaseline from '@mui/material/CssBaseline'
-import  { theme } from './theme'
-import { Box } from '@mui/material'
-import Header from './components/header'
+import App from './App'
 
 
 
 createRoot(document.getElementById('root')!).render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <Header></Header>
-  </ThemeProvider>
+  <StrictMode>
+    <App />
+  </StrictMode>
 )
