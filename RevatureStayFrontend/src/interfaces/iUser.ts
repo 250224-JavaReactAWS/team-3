f32ae32281d7266c905b73834c625579c4defdc2
crisscde <cristophercuautzo@gmail.com>
2025-04-10 12:11:17 -0600

    Fix the interface for user

--- RevatureStayFrontend/src/interfaces/iUser.ts
+++ RevatureStayFrontend/src/interfaces/iUser.ts
@@ -1,16 +1,8 @@
-import { IHotel } from "./IHotel";
-import IReservation from "./iReservation";
-import IReview from "./iReviews";
-
 export default interface IUser {
   userId: number,     
   firstName: string,
   lastName: string,
   email: string,
   password: string,
-  userRole: "CUSTOMER" | "OWNER",
-  reservations: IReservation[],
-  reviews: IReview[],
-  ownerHotels: IHotel[] | null,
-  hotels: IHotel[]
+  role: "CUSTOMER" | "OWNER"
 }
