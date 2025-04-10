a40e2db5aa02d78c253c3abe2b1652eae261da7e
crisscde <cristophercuautzo@gmail.com>
2025-04-10 12:27:27 -0600

    Add a request for get the role of the user
    - Add axios dependency for make our request to the db
    - Add createContext() for check the role in every place of the page
    - Add useEffect() for make a get request and obtain the role

--- RevatureStayFrontend/src/App.tsx
+++ RevatureStayFrontend/src/App.tsx
@@ -3,17 +3,45 @@ import { ThemeProvider } from "@emotion/react";
 import { theme } from "./theme";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from "./components/Navbar";
+import Login from "./components/user/Login";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+import Hotels from "./components/HotelManagment/Hotels";
+import Register from "./components/user/Register";
+
+export interface AuthContextType {
+  role: "CUSTOMER" | "OWNER" | "UNAUTHENTICATED",
+  setRole: (role: "CUSTOMER" | "OWNER" | "UNAUTHENTICATED") => void
+}
+
+export const authContext = createContext<AuthContextType | null>(null)
 
 export default function App() {
+  const [role, setRole] = useState<"CUSTOMER" | "OWNER" | "UNAUTHENTICATED">("UNAUTHENTICATED");
+
+  useEffect(() => {
+    axios.get<"CUSTOMER" | "OWNER">("http://localhost:8080/users/session", {withCredentials:true })
+      .then(res => {
+        setRole(res.data)
+        console.log(res);
+      })
+      .catch(err => {
+        setRole("UNAUTHENTICATED");
+        console.log(err);
+  })}, [])
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          {/* <Route path="" element=""/> */}
-        </Routes>
-      </BrowserRouter>
+      <authContext.Provider value={{role, setRole}}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/hotels' element={<Hotels />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='register' element={<Register />}/>
+          </Routes>
+        </BrowserRouter>
+      </authContext.Provider>
     </ThemeProvider>
   )
 }
