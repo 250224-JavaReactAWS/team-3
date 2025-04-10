6097a7bbcafd0b839b94f4e83b28d523ab0d9983
Criss Cuautzo <cristophercuautzo@gmail.com>
2025-04-10 16:41:41 -0600

    Delete RevatureStayFrontend/src/interfaces/IRoom.ts

--- RevatureStayFrontend/src/interfaces/IRoom.ts
+++
@@ -1,8 +0,0 @@
-export interface IRoom {
-    roomId: number,
-    type: "SINGLE" | "DOUBLE" | "TRIPLE" | "SUITE",
-    beds: number,
-    baths: number,
-    price: number,
-    available: boolean
-}
