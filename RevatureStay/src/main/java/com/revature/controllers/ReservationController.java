49502da0bf2eb2d9f2e073536b6591b7b1cadf03
Aldair88e <edherderek@gmail.com>
2025-04-10 14:53:39 -0600

    Added cors list

--- RevatureStay/src/main/java/com/revature/controllers/ReservationController.java
+++ RevatureStay/src/main/java/com/revature/controllers/ReservationController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
 @RequestMapping("reservations")
+@CrossOrigin(origins = {"http://localhost:5173"}, allowCredentials = "true")
 public class ReservationController {
     private final ReservationServices reservationServices;
 
