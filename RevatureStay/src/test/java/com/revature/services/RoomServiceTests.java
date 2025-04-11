d3ed3cf96a2cfac8448f0e6969505b8077377236
crisscde <cristophercuautzo@gmail.com>
2025-04-10 23:09:51 -0600

    Add test for get rooms
    We have 3 methods and each have her own tests:
      - getRoomById()
        - givenThatRoomDoesNotExists_whenSearchById_thenThrowsRoomNotFoundException()
        - givenThatRoomDoesExists_whenSearchById_thenReturnRoom()
      - getAllAvailableRooms()
        - givenThatHotelDoesNotExists_whenGetAllAvailableRooms_thenThrowsHotelNotFoundException()
        - givenThatHotelDoesExists_whenGetAllAvailableRooms_thenReturnListOfAvailableRooms()
      - getAllRomsByHotelId()
        - givenThatHotelDoesNotExists_whenGetAllRoomsById_thenThrowsHotelNotFoundException()
        - givenThatHotelDoesExists_whenGetAllRoomsById_thenReturnListOfRooms()

--- RevatureStay/src/test/java/com/revature/services/RoomServiceTests.java
+++ RevatureStay/src/test/java/com/revature/services/RoomServiceTests.java
@@ -1,5 +1,6 @@
 package com.revature.services;
 
+import com.revature.exceptions.custom.hotel.HotelNotFoundException;
 import com.revature.exceptions.custom.room.RoomNotFoundException;
 import com.revature.models.*;
 import com.revature.repos.HotelDAO;
@@ -10,6 +11,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -71,4 +74,117 @@ public class RoomServiceTests {
     verify(mockedRoomDAO).save(roomToUpdate);
   }
 
+  // getRoomById()
+  @Test public void givenThatRoomDoesNotExists_whenSearchById_thenThrowsRoomNotFoundException () {
+    // Arrange
+    int roomId = 1;
+    when(mockedRoomDAO.findById(any())).thenReturn(Optional.empty());
+
+    // Act & Assert
+    assertThrows(RoomNotFoundException.class, () -> roomService.getRoomById(roomId));
+  }
+
+  @Test public void givenThatRoomDoesExists_whenSearchById_thenReturnRoom () {
+    // Arrange
+    int roomId = 1;
+    Room roomToUpdate = new Room(1, RoomType.SINGLE, 1, 1, 55.32, true, new Hotel());
+    when(mockedRoomDAO.findById(any())).thenReturn(Optional.of(roomToUpdate));
+
+    // Act
+    Room obtainedRoom = roomService.getRoomById(roomId);
+
+    // Assert
+    assertEquals(obtainedRoom, roomToUpdate);
+  }
+
+  // getAllAvailableRooms()
+  @Test public void givenThatHotelDoesNotExists_whenGetAllAvailableRooms_thenThrowsHotelNotFoundException () {
+    // Arrange
+    int hotelId = 1;
+    when(mockedHotelDAO.findById(hotelId)).thenReturn(Optional.empty());
+
+    // Act & Assert
+    assertThrows(HotelNotFoundException.class, () -> roomService.getAllAvailableRooms(hotelId));
+  }
+
+  @Test public void givenThatHotelDoesExists_whenGetAllAvailableRooms_thenReturnListOfAvailableRooms () {
+    // Arrange
+    int hotelId = 1;
+
+    Hotel existHotel = new Hotel(hotelId,
+                                 "test",
+                                 "test test",
+                                 "55-5555-5555",
+                                 "test description",
+                                 new User(),
+                                 new ArrayList<Room>(),
+                                 new ArrayList<Image>(),
+                                 new ArrayList<Reservation>(),
+                                 new ArrayList<Review>(),
+                                 new ArrayList<User>());
+
+    List<Room> rooms = new ArrayList<>(List.of(new Room(1, RoomType.SINGLE, 2, 2, 43.24, true, existHotel),
+                                               new Room(2, RoomType.DOUBLE, 1, 1, 55.32, false, existHotel),
+                                               new Room(3, RoomType.TRIPLE, 1, 2, 64.21, true, existHotel),
+                                               new Room(4, RoomType.SUITE, 3, 2, 95.65, false, existHotel)));
+
+    List<Room> availableRooms = new ArrayList<>(List.of(new Room(1, RoomType.SINGLE, 2, 2, 43.24, true, existHotel), new Room(3, RoomType.TRIPLE, 1, 2, 64.21, true, existHotel)));
+
+    existHotel.setRooms(rooms);
+
+    when(mockedHotelDAO.findById(hotelId)).thenReturn(Optional.of(existHotel));
+    when(mockedRoomDAO.getAllAvailableRoomsByHotelId(hotelId)).thenReturn(availableRooms);
+
+    // Act
+    List<Room> availableRoomsObtained = roomService.getAllAvailableRooms(hotelId);
+
+    // Assert
+    assertEquals(availableRooms.size(), availableRoomsObtained.size());
+    assertEquals(availableRooms, availableRoomsObtained);
+    verify(mockedHotelDAO).findById(hotelId);
+  }
+
+  // getAllRoomsByHotelId()
+  @Test public void givenThatHotelDoesNotExists_whenGetAllRoomsById_thenThrowsHotelNotFoundException () {
+    // Arrange
+    int hotelId = 1;
+    when(mockedHotelDAO.findById(hotelId)).thenReturn(Optional.empty());
+
+    // Act & Assert
+    assertThrows(HotelNotFoundException.class, () -> roomService.getAllRoomsByHotelId(hotelId));
+  }
+
+  @Test public void givenThatHotelDoesExists_whenGetAllRoomsById_thenReturnListOfRooms () {
+    // Arrange
+    int hotelId = 1;
+
+    Hotel existHotel = new Hotel(hotelId,
+                                 "test",
+                                 "test test",
+                                 "55-5555-5555",
+                                 "test description",
+                                 new User(),
+                                 new ArrayList<Room>(),
+                                 new ArrayList<Image>(),
+                                 new ArrayList<Reservation>(),
+                                 new ArrayList<Review>(),
+                                 new ArrayList<User>());
+
+    List<Room> rooms = new ArrayList<>(List.of(new Room(1, RoomType.SINGLE, 2, 2, 43.24, true, existHotel),
+                                               new Room(2, RoomType.DOUBLE, 1, 1, 55.32, false, existHotel),
+                                               new Room(3, RoomType.TRIPLE, 1, 2, 64.21, true, existHotel),
+                                               new Room(4, RoomType.SUITE, 3, 2, 95.65, false, existHotel)));
+
+    existHotel.setRooms(rooms);
+
+    when(mockedHotelDAO.findById(hotelId)).thenReturn(Optional.of(existHotel));
+
+    // Act
+    List<Room> roomsObtained = roomService.getAllRoomsByHotelId(hotelId);
+
+    // Assert
+    assertEquals(rooms.size(), roomsObtained.size());
+    assertEquals(rooms, roomsObtained);
+    verify(mockedHotelDAO).findById(hotelId);
+  }
 }
